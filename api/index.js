dd234272e397c04d999a5ed063e441db0af9a5ec
Donalddddddd <161224579+Donalddddddd@users.noreply.github.com>
2025-11-27 00:25:10 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -59,7 +59,8 @@ const loveQuotes = [
     "My world is infinitely better with you in it. Dear baby jana, you are my blessing.",
     "My world is infinitely better with you in it. Dear baby jana, you are my blessing.",
     "You are the missing piece to the puzzle of my life. Dear baby jana, with you, I am whole.",
-    "You are the missing piece to the puzzle of my life. Dear baby jana, with you, I am whole."
+    "You are the missing piece to the puzzle of my life. Dear baby jana, with you, I am whole.",
+    "You saw the mess I am and still chose to love me. Dear baby jana, you are my acceptance."
 ];
 
 // Function to get quote of the day (based on date)
