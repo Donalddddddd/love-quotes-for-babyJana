62cd5e7c58ce0db0912619b75aa2920516447e1a
Donalddddddd <161224579+Donalddddddd@users.noreply.github.com>
2025-11-29 09:15:30 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -61,6 +61,7 @@ const loveQuotes = [
     "You are the missing piece to the puzzle of my life. Dear baby jana, with you, I am whole.",
     "You are the missing piece to the puzzle of my life. Dear baby jana, with you, I am whole.",
     "You saw the mess I am and still chose to love me. Dear baby jana, you are my acceptance.",
+    "You saw the mess I am and still chose to love me. Dear baby jana, you are my acceptance.",
     "You saw the mess I am and still chose to love me. Dear baby jana, you are my acceptance."
 ];
 
